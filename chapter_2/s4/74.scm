f8a02a0150bb3a79aaf66e64814317d4a1a13b29
Jerome Boisvert-Chouinard <jerome.boisvertchouinard@savoirfairelinux.com>
2017-03-16 10:04:49 -0400

    wip 2.74

--- chapter_2/s4/74.scm
+++ chapter_2/s4/74.scm
@@ -1,2 +1,12 @@
 ; SICP Exercise 2.74
 
+(load-module "sicp.table")
+
+(define opstable (make-table))
+(define get (table-make-get opstable))
+(define put (table-make-put opstable))
+
+; a.
+(define (get-record employee-name file)
+  ((get 'get-record (list employee-name file))))
+; The 
