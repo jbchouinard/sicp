7ba8722c316a77ad2d8b10fe39c91c2a55c4e08d
Jerome Boisvert-Chouinard <jerome.boisvertchouinard@savoirfairelinux.com>
2017-03-16 18:04:55 -0400

    exercise 2.74

--- chapter_2/s4/74.scm
+++ chapter_2/s4/74.scm
@@ -6,7 +6,71 @@
 (define get (table-make-get opstable))
 (define put (table-make-put opstable))
 
-; a.
+; Each division gets a unique type tag, and are responsible for implementing
+; file and record access methods and registering them using put.
+(define (attach-tag tag content)
+  (cons tag content))
+
+(define (get-tag item)
+  (car item))
+
+(define (get-content item)
+  (cdr item))
+
+; a. implement get-record
 (define (get-record employee-name file)
-  ((get 'get-record (list employee-name file))))
-; The 
+  (let ((tag (get-tag file)))
+    (attach-tag tag
+      ((get 'get-record tag) employee-name (get-content file)))))
+
+; b. implement get-salary
+(define (get-information key record)
+  ((get 'get-information (get-tag record)) key (get-content record)))
+
+(define (get-salary employee-name file)
+  (get-information 'salary
+                   (get-record employee-name
+                               file)))
+
+; c. implement find-employee-record
+(define (find-employee-record employee-name files)
+  (define (find-iter remaining)
+    (if (null? remaining)
+      false
+      (let ((record (get-record employee-name (car files))))
+        (if (equal? false record)
+          (find-iter (cdr files))
+          record))))
+  (find-iter files))
+
+; d. Assuming the records at the company have the same general structure
+; (file containing a set of records, record containing a set of attribute),
+; a tag must be assigned for each file type at the new company, and accessor
+; methods must be implemented and added to the central system for each file
+; type.
+
+; Example implementation of a divsion file package
+(define (install-example-division-package)
+  ; Implementation
+  (define (get-record employee-name file)
+    (table-get file employee-name false))
+  (define (get-information key record)
+    (table-get record key false))
+  ; Interface
+  (put 'get-record 'example get-record)
+  (put 'get-information 'example get-information)
+  'done)
+
+; Example division file
+(define (make-file-example)
+  (attach-tag 'example (make-table)))
+(define file-example (make-file-example))
+(table-rec-set! (get-content file-example)
+                (list "Patrick Bateman" 'salary)
+                150000)
+(table-rec-set! (get-content file-example)
+                (list "Patrick Bateman" 'address)
+                "55 Wall St., New York, NY, US")
+(table-rec-set! (get-content file-example)
+                (list "Patrick Bateman" 'position)
+                "Vice-president")
